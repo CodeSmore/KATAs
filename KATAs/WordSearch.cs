1bffc8d725169af304daaedc84c0d1605e8f70d6
Peter Kirk <codesmore@yahoo.com>
2019-01-28 10:34:51 -0500

    WordSearch - Test001 passes with magic string

--- KATAs/WordSearch.cs
+++ KATAs/WordSearch.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace KATAs
 {
-    class WordSearch
+    public class WordSearch
     {
+        public string GetInput(string fileName)
+        {
+            string fileContentsAsString = "SEEK,TEST,WORD B,V,L,T,W X,V,S,O,O S,E,E,K,R T,P,W,X,D K,T,E,S,D";
+
+            return fileContentsAsString;
+        }
     }
 }
