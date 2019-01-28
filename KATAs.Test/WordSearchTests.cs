ecc53e1e1a4823c11b1a6a0f5344c0eebd64bef3
Peter Kirk <codesmore@yahoo.com>
2019-01-28 10:43:48 -0500

    WordSearch - Test002_GetInputReturnsTheStringOfTextFromInputTextFileTestDocument002, red

--- KATAs.Test/WordSearchTests.cs
+++ KATAs.Test/WordSearchTests.cs
@@ -16,5 +16,15 @@ namespace KATAs.Test
 
             Assert.AreEqual("SEEK,TEST,WORD B,V,L,T,W X,V,S,O,O S,E,E,K,R T,P,W,X,D K,T,E,S,D", wordSearch.GetInput("TestDocument001"));
         }
+
+
+        [TestMethod]
+        public void Test002_GivenWordSearch_GetInputReturnsTheStringOfTextFromInputTextFileTestDocument002()
+        {
+            WordSearch wordSearch = new WordSearch();
+
+            Assert.AreEqual("THIS,WAS,JUST,THE,TEST,FILE L,T,C,T,V,L,H D,D,S,L,S,Q,K F,U,T,E,S,T,O J,I,T,X,Y,S,C S,H,L,Y,W,I,N E,M,L,E,A,H,J Q,I,R,K,S,T,I"
+                , wordSearch.GetInput("TestDocument002"));
+        }
     }
 }
