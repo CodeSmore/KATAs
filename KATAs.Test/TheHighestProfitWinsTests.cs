8f207630a83878c134d48efcc83dda45ecd47056
Peter Kirk <codesmore@yahoo.com>
2019-03-14 09:54:24 -0400

    TheHighestProfitWins - Added 2 more test cases to Test001, both passing and green

--- KATAs.Test/TheHighestProfitWinsTests.cs
+++ KATAs.Test/TheHighestProfitWinsTests.cs
@@ -10,13 +10,25 @@ namespace KATAs.Test
     public class TheHighestProfitWinsTests
     {
         [TestMethod]
-        public void Test001_GivenTheHighestProfitWins_()
+        public void Test001_GivenTheHighestProfitWins_WhenInputIsAnArrayOfInts_ThenOutputIsAnArrayOfIntsWithLowestAndHighestValueFromInputArray()
         {
             TheHighestProfitWins highestProfitWins = new TheHighestProfitWins();
 
             int[] input = new int[] { 1, 2, 5, -1, 12, 20 };
 
             CollectionAssert.AreEqual(new int[] { -1, 20 }, highestProfitWins.MinMax(input));
+
+            // ----------------------------------------------------------------------------------
+
+            input = new int[] { 2334454, 5 };
+
+            CollectionAssert.AreEqual(new int[] { 5, 2334454 }, highestProfitWins.MinMax(input));
+
+            // ----------------------------------------------------------------------------------
+
+            input = new int[] { 1 };
+
+            CollectionAssert.AreEqual(new int[] { 1, 1 }, highestProfitWins.MinMax(input));
         }
     }
 }
