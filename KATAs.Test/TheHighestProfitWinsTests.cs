9f33c90371a2013c7df9c384cef6fe4a32304d0e
Peter Kirk <codesmore@yahoo.com>
2019-03-14 09:51:10 -0400

    TheHighestProfitWins - Test001_GivenTheHighestProfitWins_WhenInputIsAnArrayOfInts_ThenOutputIsAnArrayOfIntsWithLowestAndHighestValueFromInputArray added and green

---
+++ KATAs.Test/TheHighestProfitWinsTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace KATAs.Test
+{
+    [TestClass]
+    public class TheHighestProfitWinsTests
+    {
+        [TestMethod]
+        public void Test001_GivenTheHighestProfitWins_()
+        {
+            TheHighestProfitWins highestProfitWins = new TheHighestProfitWins();
+
+            int[] input = new int[] { 1, 2, 5, -1, 12, 20 };
+
+            CollectionAssert.AreEqual(new int[] { -1, 20 }, highestProfitWins.MinMax(input));
+        }
+    }
+}
---
+++ KATAs/TheHighestProfitWins.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KATAs
+{
+    public class TheHighestProfitWins
+    {
+        public int[] MinMax(int[] arrayOfCosts)
+        {
+            int min = 0, max = 0;
+
+            min = max = arrayOfCosts[0];
+
+            foreach (int cost in arrayOfCosts)
+            {
+                if (min > cost)
+                {
+                    min = cost;
+                }
+
+                if (max < cost)
+                {
+                    max = cost;
+                }
+            }
+
+            return new int[] { min, max };
+        }
+    }
+}
