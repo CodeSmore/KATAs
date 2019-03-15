1357b8a5e8efc3c65cf40436dd53009763b1d232
Peter Kirk <codesmore@yahoo.com>
2019-03-15 09:21:20 -0400

    DetectPangram - Test001_GivenDetectPangram_WhenInputIsNotAPangram_ThenIsPangramReturnsFalse added, green due to static bool return

---
+++ KATAs.Test/DetectPangramTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KATAs.Test
+{
+    [TestClass]
+    public class DetectPangramTests
+    {
+        [TestMethod]
+        public void Test001_GivenDetectPangram_WhenInputIsNotAPangram_ThenIsPangramReturnsFalse()
+        {
+            string input = "Not a pangram.";
+
+            Assert.IsFalse(DetectPangram.IsPangram(input));
+        }
+    }
+}
---
+++ KATAs/DetectPangram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KATAs
+{
+    public class DetectPangram
+    {
+        public static bool IsPangram(string str)
+        {
+            return false;
+        }
+    }
+}
