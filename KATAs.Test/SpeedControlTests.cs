41f485842f906dcb09b51323e6398c342f73be34
Peter Kirk <codesmore@yahoo.com>
2019-03-20 09:36:39 -0400

    SpeedControl - Test002_WhenTwoDistanceSamplesExist_ThenReturnTheCorrectSpeed added, TestHelperMethod001_WhenGetDistanceSegmentsHelperMethodIsCalled_ThenReturnTheDistanceBetweenEachSegment and TestHelperMethod002_WhenGetSegmentsAverageSpeedsMethodIsCalled_ThenReturnTheFloorIntegerAverageSpeedPerSegment added to accomodate Test002. All pass

--- KATAs.Test/SpeedControlTests.cs
+++ KATAs.Test/SpeedControlTests.cs
@@ -20,6 +20,41 @@ namespace KATAs.Test
             int actualResult = SpeedControl.GetMaxAverageSpeedPerHour(seconds, distanceSamples);
 
             Assert.AreEqual(expectedResult, actualResult);
-        }  
+        }
+
+        [TestMethod]
+        public void Test002_GivenSpeedControl_WhenTwoDistanceSamplesExist_ThenReturnTheCorrectSpeed()
+        {
+            double[] distanceSamples = new double[] { 0.0, 0.19 };
+            int seconds = 15;
+
+            int expectedResult = 45;
+            int actualResult = SpeedControl.GetMaxAverageSpeedPerHour(seconds, distanceSamples);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestHelperMethod001_GivenSpeedControl_WhenGetDistanceSegmentsHelperMethodIsCalled_ThenReturnTheDistanceBetweenEachSegment()
+        {
+            double[] distanceSamples = new double[] { 0.0, 0.19, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 2.25 };
+
+            double[] expectedResult = new double[] { 0.19, 0.31, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25 };
+            double[] actualResult = SpeedControl.GetDistanceSegments(distanceSamples);
+
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
+
+        [TestMethod]
+        public void TestHelperMethod002_GivenSpeedControl_WhenGetSegmentsAverageSpeedsMethodIsCalled_ThenReturnTheFloorIntegerAverageSpeedPerSegment()
+        {
+            double[] distanceSegments = new double[] { 0.19, 0.31, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.25 };
+            int seconds = 15;
+
+            int[] expectedResult = new int[] { 45, 74, 60, 60, 60, 60, 60, 60, 60 };
+            int[] actualResult = SpeedControl.GetSegmentsAverageSpeeds(distanceSegments, seconds);
+
+            CollectionAssert.AreEqual(expectedResult, actualResult);
+        }
     }
 }
--- KATAs/SpeedControl.cs
+++ KATAs/SpeedControl.cs
@@ -6,11 +6,55 @@ namespace KATAs
 {
     public class SpeedControl
     {
-        public static int GetMaxAverageSpeedPerHour(int s, double[] x)
+        public static int GetMaxAverageSpeedPerHour(int seconds, double[] distanceSamples)
         {
             int result = 0;
 
+            if (distanceSamples.Length < 2)
+            {
+                return 0;
+            }
+
+            double[] distanceSegments = GetDistanceSegments(distanceSamples);
+
+            int[] segmentsAverageSpeeds = GetSegmentsAverageSpeeds(distanceSegments, seconds);
+
+            foreach (int speed in segmentsAverageSpeeds)
+            {
+                if (speed > result)
+                {
+                    result = speed;
+                }
+            }
+
             return result;
+        }
+
+        public static int[] GetSegmentsAverageSpeeds(double[] distanceSegments, int seconds)
+        {
+            int[] averageSpeedOfEachSegment = new int[distanceSegments.Length];
+
+            for (int i = 0; i < distanceSegments.Length; ++i)
+            {
+                averageSpeedOfEachSegment[i] = (int)Math.Floor((3600 * distanceSegments[i]) / seconds);
+            }
+
+            return averageSpeedOfEachSegment;
+        }
+
+        public static double[] GetDistanceSegments(double[] distanceSamples)
+        {
+            double[] distanceSegments = new double[distanceSamples.Length - 1];
+
+            double previousDistance = distanceSamples[0];
+            for (int i = 1; i < distanceSamples.Length; ++i)
+            {
+                distanceSegments[i - 1] = distanceSamples[i] - previousDistance;
+
+                previousDistance = distanceSamples[i];
+            }
+
+            return distanceSegments;
         }
     }
 }
