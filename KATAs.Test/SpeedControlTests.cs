093ea62cb20cedbffea29d6c7723e9d4c3382583
Peter Kirk <codesmore@yahoo.com>
2019-03-20 09:04:30 -0400

    SpeedControl - Test001_WhenLessThanTwoDistanceSamplesExist_ThenZeroIsReturned added and passes

---
+++ KATAs.Test/SpeedControlTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace KATAs.Test
+{
+    [TestClass]
+    public class SpeedControlTests
+    {
+        [TestMethod]
+        public void Test001_GivenSpeedControl_WhenLessThanTwoDistanceSamplesExist_ThenZeroIsReturned()
+        {
+            double[] distanceSamples = new double[] { 0.0 };
+            int seconds = 25;
+
+            int expectedResult = 0;
+            int actualResult = SpeedControl.GetMaxAverageSpeedPerHour(seconds, distanceSamples);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }  
+    }
+}
---
+++ KATAs/SpeedControl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KATAs
+{
+    public class SpeedControl
+    {
+        public static int GetMaxAverageSpeedPerHour(int s, double[] x)
+        {
+            int result = 0;
+
+            return result;
+        }
+    }
+}
+
